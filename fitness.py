9e002f9b2e08ae71b0a6980dc329b77c052c2d72
Asa Mcdaniel <95445304+asamc@users.noreply.github.com>
2026-03-28 15:30:03 -0500

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -1,13 +1,18 @@
-# fitness.py
+### fitness.py
 # 
 # This is a fitness function for quickly executing a lot of candidate python code pieces and evaluating time taken.
+# The candidate files should be named candidateNUM.py, where NUM is from 0 to NUM_OF_CANDIDATES - 1
 # The usual way to call this code is as follows.
 # 
-# ~> python fitness.py candidate NUM_OF_CANDIDATES
+# ~> python fitness.py genericCandidateName NUM_OF_CANDIDATES
 #
-# If the code you are trying to execute needs to be run in a bigger project, use the following code with dummy names replaced.
+# If the code you are trying to execute needs to be run in a bigger project, use the following code with dummy names replaced 
+# for each candidate name.
 # 
-# ~> python -m cProfile ./yourFileName.py > outputFileName
+# ~> python -m cProfile ./candidateName.py > outputFileName
+#
+# The expected output of this file is a bunch of data files that can be read on their own to find weaknesses, and there will be 
+# a visualizer to show the data in an easily readable way. An example of an output file is in the repository called candidate_Result_0
 
 import os
 import sys
